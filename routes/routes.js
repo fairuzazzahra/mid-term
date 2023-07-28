15dd1fb2849e6947081fb109dd89f551e6b40942
fairuzazzahra <56762644+fairuzazzahra@users.noreply.github.com>
2023-07-28 09:20:19 +0700

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -12,7 +12,7 @@ router.get("/product", async (req, res) => {
     // db.product.find()
     const allProducts = await Product.find();
     res.status(200).json({
-      songs: allProducts,
+      products: allProducts,
     });
   } catch (err) {
     res.status(500).json({
